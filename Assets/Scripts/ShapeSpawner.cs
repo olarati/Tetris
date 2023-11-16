21b707d61f169b279b577614745a2676d07fa2a5
Kudrman <kudrman_93@mail.ru>
2023-11-16 12:31:17 +0700

    поправлено имя в спаунере

--- Assets/Scripts/ShapeSpawner.cs
+++ Assets/Scripts/ShapeSpawner.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class ShapeSpawner : MonoBehaviour
 {
-    public Shape[] shapePrefabs = new Shape[0];
+    public Shape[] ShapePrefabs = new Shape[0];
 
     public Shape SpawnNextShape()
     {
-        Shape randomPrefab = shapePrefabs[Random.Range(0, shapePrefabs.Length)];
+        Shape randomPrefab = ShapePrefabs[Random.Range(0, ShapePrefabs.Length)];
         return Instantiate(randomPrefab);
     }
     
