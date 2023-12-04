0ce39981b13461cdf024403901be460761cbfe56
Kudrman <kudrman_93@mail.ru>
2023-12-04 19:14:47 +0700

    - добавлена анимация инзменения значения очков

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -3,12 +3,18 @@ using TMPro;
 
 public class Score : MonoBehaviour
 {
+    public float ShownScoreChangeDuration = 0.2f;
+
     private const string BestScoreKey = "BestScore";
 
     private TextMeshProUGUI _scoreText;
     private Animation _scoreIncreaseAnimation; 
     private int _score;
     private int _bestScore;
+    private int _shownScore;
+    private int _shownScoreDelta;
+    private bool _shownScoreChangeAnimationActive;
+    private float _shownScoreChangeTimer;
 
     // Добавляем очки
     public void AddScore(int value)
@@ -21,6 +27,7 @@ public class Score : MonoBehaviour
     public void Restart()
     {
         SetScore(0);
+        SetShownScore(0);
     }
 
     // Получаем текущий счёт
@@ -46,7 +53,7 @@ public class Score : MonoBehaviour
     private void Start()
     {
         FillComponents();
-        SetScore(0);
+        Restart();
         LoadBestScore();
     }
 
@@ -61,7 +68,7 @@ public class Score : MonoBehaviour
     private void SetScore(int value)
     {
         _score = value;
-        SetScoreText(value);
+        ActivateShownScoreChangeAnimation();
     }
 
     // Отображаем текст с очками
@@ -84,5 +91,46 @@ public class Score : MonoBehaviour
     private void PlayScoreIncreaseAnimation()
     {
         _scoreIncreaseAnimation.Play(PlayMode.StopAll);
+    }
+
+    private void ActivateShownScoreChangeAnimation()
+    {
+        _shownScoreChangeAnimationActive = true;
+        _shownScoreChangeTimer = 0;
+        _shownScoreDelta = _score - _shownScore;
+    }
+
+    private void DeactivateShownScoreChangeAnimation()
+    {
+        _shownScoreChangeAnimationActive = false;
+    }
+
+    private void Update()
+    {
+        ShownScoreChangeAnimation();
+    }
+
+    private void ShownScoreChangeAnimation()
+    {
+        if(!_shownScoreChangeAnimationActive)
+        {
+            return;
+        }
+
+        _shownScoreChangeTimer += Time.deltaTime / ShownScoreChangeDuration;
+        int nextShownScore = (int) Mathf.Lerp(_shownScore,_score, _shownScoreChangeTimer);
+
+        SetShownScore(nextShownScore);
+
+        if(_shownScoreChangeTimer >= 1)
+        {
+            DeactivateShownScoreChangeAnimation();
+        }
+    }
+
+    private void SetShownScore(int value)
+    {
+        _shownScore = value;
+        SetScoreText(value);
     }
 }
